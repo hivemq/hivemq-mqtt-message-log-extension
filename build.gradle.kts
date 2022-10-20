f2d47fcfed426dcb1e5c441930ea7d6ba9b0c287
LukasHiveMQ <lukas.brand@hivemq.com>
2022-10-20 09:17:02 +0200

    Applied review.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ hivemqExtension {
     name.set("HiveMQ Mqtt Message Log Extension")
     author.set("HiveMQ")
     priority.set(1000)
-    startPriority.set(10000)
+    startPriority.set(1000)
     sdkVersion.set("${property("hivemq-extension-sdk.version")}")
 }
 
@@ -43,7 +43,6 @@ dependencies {
     testImplementation("ch.qos.logback:logback-classic:${property("logback.version")}")
     testImplementation("org.junit.jupiter:junit-jupiter:${property("junit-jupiter.version")}")
     testImplementation("org.mockito:mockito-core:${property("mockito.version")}")
-    testImplementation("org.mockito:mockito-junit-jupiter:${property("mockito.version")}")
 }
 
 tasks.withType<Test>().configureEach {
