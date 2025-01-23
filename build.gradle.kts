8e911ce33e96e83375cfe3e2ca2793141ce02f3b
Silvio Giebl <silvio.giebl@hivemq.com>
2025-01-23 15:07:23 +0100

    Update gradle-oci to 0.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ oci {
                 runtime("com.hivemq:hivemq-enterprise:latest") { isChanging = true }
             }
             layers {
-                layer("hivemqExtension") {
+                layer("main") {
                     contents {
                         permissions("opt/hivemq/", 0b111_111_101)
                         permissions("opt/hivemq/extensions/", 0b111_111_101)
